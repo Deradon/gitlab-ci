858b20b8502f343405dce47b6e62b206df85867c
Patrick Helm <deradon87@gmail.com>
2013-03-30 23:47:35 +0100

    Change migrations to work with PostgreSQL
    Tested with PostgreSQL 9.1

--- db/migrate/20121115094430_increate_trace_colunm_limit.rb
+++ db/migrate/20121115094430_increate_trace_colunm_limit.rb
@@ -1,6 +1,12 @@
 class IncreateTraceColunmLimit < ActiveRecord::Migration
   def up
-    change_column :builds, :trace, :text, :limit => 4294967295
+    # NOTE: PostgreSQL does not allow :limit for :text
+    case ActiveRecord::Base.connection.adapter_name
+    when 'PostgreSQL'
+      change_column :builds, :trace, :text
+    else
+      change_column :builds, :trace, :text, :limit => 4294967295
+    end
   end
 
   def down
--- db/migrate/20130114153451_change_schedule_invertal.rb
+++ db/migrate/20130114153451_change_schedule_invertal.rb
@@ -1,6 +1,18 @@
 class ChangeScheduleInvertal < ActiveRecord::Migration
   def up
-    change_column :projects, :polling_interval, :integer, null: true
+    # NOTE: PostgreSQL can not cast :string/:text to :integer implicity,
+    #       So we need to do a workaround.
+    case ActiveRecord::Base.connection.adapter_name
+    when 'PostgreSQL'
+      rename_column :projects, :polling_interval, :polling_interval_string
+      add_column :projects, :polling_interval, :integer, null: true
+
+      Project.reset_column_information
+      Project.find_each { |c| c.update_attribute(:polling_interval, c.polling_interval_string) }
+      remove_column :projects, :polling_interval_string
+    else
+      change_column :projects, :polling_interval, :integer, null: true
+    end
   end
 
   def down
